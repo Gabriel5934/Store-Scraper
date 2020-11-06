565d50a2c3c2de3845b05bcc774ee92b63b294a3
Gabriel5934 <gabriel.andrade@outlook.com.br>
2020-11-06 17:40:36 -0300

    Changed the scanning method

--- storeScraperAPI.py
+++ storeScraperAPI.py
@@ -8,75 +8,83 @@ load_dotenv()
 key = os.getenv("API_KEY")
 
 def getCoordinate(street):
-    street = street + os.getenv("CITY")
-    number = 5000
-    coords = []
+    input = street + os.getenv("CITY")
+    formattedInput = input.replace(" ", "%20")
+    fields = "name,geometry"
 
-    while number != 0:
-        input = street + " " + str(number)
-        formattedInput = input.replace(" ", "%20")
-        fields = "name,geometry"
-
-        resp = requests.get(url=f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={key}&input={formattedInput}&inputtype=textquery&fields={fields}")
-        
-        coords.append(resp.json()["candidates"][0]["geometry"]["location"])
-        streetName = (resp.json()["candidates"][0]["name"])
-
-        if number == 500:
-            number = 1
-        elif number == 1:
-            number -= 1
-        else:
-            number -= 500
-
-    coordsNoRepeat = []
-    for i in range(len(coords)):
-        if coords[i] not in coordsNoRepeat:
-            coordsNoRepeat.append(coords[i])
+    resp = requests.get(url=f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={key}&input={formattedInput}&inputtype=textquery&fields={fields}")
     
-    return(coordsNoRepeat, streetName)
+    coords = (resp.json()["candidates"][0]["geometry"]["location"])
+    streetName = (resp.json()["candidates"][0]["name"])
+    
+    return(coords)
 
 userStreetName = str(input("Insira a rua para escanear: "))
 userNeighborhood = str(input("Insira o bairro: "))
 userStreet = userStreetName + " " + userNeighborhood 
 spots = getCoordinate(userStreet)
-radius = "1000"
 
 placesFound = []
+number = 5000
 
-for i in range(len(spots[0])):
-    resp = requests.get(url=f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={key}&location={spots[0][i]['lat']},%20{spots[0][i]['lng']}&radius={radius}")
+while number != 0:
+    resp = requests.get(url=f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={key}&location={spots['lat']},%20{spots['lng']}&radius={number}")
     for i in range(len(resp.json()["results"])):
         placesFound.append({
             "name": resp.json()["results"][i]["name"],
             "location": resp.json()["results"][i]["vicinity"]
         })
         
+    if number == 500:
+        number = 1
+    elif number == 1:
+        number -= 1
+    else:
+        number -= 500
+
 if len(placesFound) == 0:
     print("Não há comércios nessa região")
 else:
-    aux = 0
+    placesFoundNoRepeat = []
     for i in range(len(placesFound)):
-        db = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = os.getenv("DB_PASSWORD"),
-            database = os.getenv("DB"),
-            auth_plugin = "mysql_native_password"
-        )
+        if placesFound[i] not in placesFoundNoRepeat:
+            placesFoundNoRepeat.append(placesFound[i])
 
+    for i in range(len(placesFoundNoRepeat)):
+        print(placesFoundNoRepeat[i]["name"])
+    
+    print(len(placesFoundNoRepeat))
+
+    db = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = os.getenv("DB_PASSWORD"),
+        database = os.getenv("DB"),
+        auth_plugin = "mysql_native_password"
+    )
+
+    aux = 0
+    for i in range(len(placesFoundNoRepeat)):
         cursor = db.cursor()
+        currentName = placesFoundNoRepeat[i]['name'].replace(" ", "_").replace("'", "")
+        currentStreet = placesFoundNoRepeat[i]['location'].replace(" ", "_").replace("'", "").split(",")[0]
+        currentLocation = placesFoundNoRepeat[i]['location'].replace(" ", "_").replace("'", "")
 
-        for i in range(len(placesFound)):
-            currentName = placesFound[i]['name'].replace(" ", "_").replace("'", "")
-            currentStreet = placesFound[i]['location'].replace(" ", "_").replace("'", "").split(",")[0]
-            currentLocation = placesFound[i]['location'].replace(" ", "_").replace("'", "")
+        cursor.execute(f"INSERT INTO places2 (place_name, place_street, place_location) VALUES ('{currentName}', '{currentStreet}', '{currentLocation}')")
+        db.commit()
+        aux += 1
 
-            cursor.execute(f"INSERT INTO places (place_name, place_street, place_location) VALUES ('{currentName}', '{currentStreet}', '{currentLocation}')")
-            db.commit()
-            aux += 1
-
-    print(f"Registrado {aux} lugares no banco de dados")
+    cursor = db.cursor()
+    cursor.execute('''
+        DELETE t1 FROM places2 t1
+        INNER JOIN places2 t2 
+        WHERE 
+            t1.place_id < t2.place_id AND 
+            t1.place_name = t2.place_name;
+    ''')
+    db.commit()
+        
+    print(f"Registrado {aux - cursor.rowcount} lugares no banco de dados")
 
 
 
